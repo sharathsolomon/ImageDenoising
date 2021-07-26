95a8dfe724abf03d4c5e0cb8f7b4a0aec23cd18e
sharathsolomon <85414148+sharathsolomon@users.noreply.github.com>
2021-07-26 21:11:09 +0530

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,11 @@ def main():
         )
         
     #readme_text = st.markdown(get_file_content_as_string("README.md"))
-    
+    if selected_box = 'About the App':
+        base64_pdf = base64.b64encode(Readme_app.pdf.read()).decode('utf-8')
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
+        st.markdown(pdf_display, unsafe_allow_html=True)
+        
     if selected_box == 'Use the App':
         #readme_text.empty()
         models()
