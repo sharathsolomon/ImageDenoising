28a5f08b0c657088dc098c04dc19e14fca9b747b
sharathsolomon <85414148+sharathsolomon@users.noreply.github.com>
2021-07-26 15:24:04 +0530

    Update model.py

--- model.py
+++ model.py
@@ -43,6 +43,7 @@ def models():
             file_bytes = np.asarray(bytearray(image.read()), dtype=np.uint8)
             nsy_img = cv2.imdecode(file_bytes, 1)
             #nsy_img = cv2.imread(image)
+            nsy_img = cv2.cvtColor(nsy_img, cv2.COLOR_BGR2RGB)
             st.image(nsy_img)
             prediction(nsy_img)
         else:
