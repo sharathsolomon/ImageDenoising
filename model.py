06373daa076eee244de5b0e1937610b307465749
sharathsolomon <85414148+sharathsolomon@users.noreply.github.com>
2021-07-27 15:41:02 +0530

    Update model.py

--- model.py
+++ model.py
@@ -56,14 +56,16 @@ def get_model():
     return RIDNet
 
 def prediction(img):
-    model = get_model()
+    state = st.text('\n Please wait while the model denoise the image.....')
+    progress_bar = st.progress(0)
     start = time.time()
+    model = get_model()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     nsy_img = cv2.resize(img,(1024,1024))
     nsy_img = nsy_img.astype("float32") / 255.0
 
     img_patches = patches(nsy_img,256)
-    
+    progress_bar.progress(30)
     nsy=[]
     for i in range(4):
         for j in range(4):
@@ -71,6 +73,7 @@ def prediction(img):
     nsy = np.array(nsy)
     
     pred_img = model.predict(nsy)
+    progress_bar.progress(70)
     pred_img = np.reshape(pred_img,(4,4,1,256,256,3))
     pred_img = unpatchify(pred_img, nsy_img.shape)
     end = time.time()
@@ -89,7 +92,10 @@ def prediction(img):
     ax[1].title.set_text("Predicted Image")
     
     st.pyplot(fig)
+    progress_bar.progress(100)
     st.write('Time taken for prediction :', str(round(end-start,3))+' seconds')
+    progress_bar.empty()
+    state.text('\n Completed!')
     
 if __name__ == "__main__":
     main()   
